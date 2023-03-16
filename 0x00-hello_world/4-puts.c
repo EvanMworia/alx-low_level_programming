54d0efccfe38070fee11ecafe60032eb4f805a2d
EvanMworia <evannji99@gmail.com>
2023-03-16 10:07:05 -0700

    C program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * Main - Entry point of the program
+ *
+ * Return - always returns a 0 in case of succcess
+ */
+int main(void)
+{
+	puts(""Programming is like building a multilingual puzzle");
+	return(0);
+}
