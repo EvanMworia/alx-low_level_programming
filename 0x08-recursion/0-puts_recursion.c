a9f72d0f74b57daaf273a9a05df89dd6779277cd
EvanMworia <evannji99@gmail.com>
2023-04-06 04:10:35 -0700

    0-puts_recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,12 +7,13 @@
  */
 void _puts_recursion(char *s)
 {
-if (*s)
-{
-_putchar(*s);
-_putchar(s + 1);
-}
-else
+	if (*s == '\0')
+	{
 
-	_putchar('\n');
+		_putchar('\n');
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
+
+
 }
